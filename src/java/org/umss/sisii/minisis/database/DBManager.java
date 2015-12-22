1b28739c9247ef133c699605c8bb30f081231383
AlexandraRodriguez <ale.tunn9095@gmail.com>
2015-12-22 14:21:44 -0400

    Database update: Task Table added

--- src/java/org/umss/sisii/minisis/database/DBManager.java
+++ src/java/org/umss/sisii/minisis/database/DBManager.java
@@ -7,16 +7,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.List;
+import java.util.ArrayList;
+import org.umss.sisii.minisis.model.Task;
 
 /**
  *
- * @author Alexandra
+ * @author PC_
  */
 public class DBManager {
 
@@ -24,65 +21,72 @@ public class DBManager {
     private ResultSet rs = null;
     private Statement s = null;
     private static DBManager manager = null;
-    
-    private DBManager(){
+    private final String url;
+    private final String password;
+    private final String postgresUser;
+
+    private DBManager() {
+        url = "jdbc:postgresql://localhost:5432/MiniSis2";
+        password = "1234";
+        postgresUser = "postgres";
         conectar();
     }
-    
-    public static DBManager getInstance(){
-        if(manager == null) {
-         manager = new DBManager();
-      }
-      return manager;
+
+    public static DBManager getInstance() {
+        if (manager == null) {
+            manager = new DBManager();
+        }
+        return manager;
     }
-   
-    private void conectar(){
-    if(connection != null){
+
+    private void conectar() {
+        if (connection != null) {
             return;
         }
-        String url = "jdbc:postgresql://localhost:5432/mini_sis_database";
-        String password = "postgres";
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(url, "postgres", password);
-            if(connection != null){
-            System.out.println("Conectando a Base de Datos...");
+            connection = DriverManager.getConnection(url, postgresUser, password);
+            if (connection != null) {
+                s = connection.createStatement();
+                System.out.println("Conectando a Base de Datos...");
             }
         } catch (Exception e) {
             System.out.println("Problemas de Conexion");
         }
     }
-    
-    
-    public boolean existsUser(String user){
-        boolean found = false;
+
+    public boolean checkUser(String user, String password) {
+        boolean isCorrect = false;
         try {
-            //conectar();
-            s = connection.createStatement();
-            rs = s.executeQuery("SELECT user_name FROM \"USER\";");
-            while(rs.next() && !found){
-                found = ( rs.getString("user_name") ).equals(user);
+            rs = s.executeQuery("SELECT password FROM \"USER\" WHERE user_name = '" + user + "';");
+            while (rs.next()) {
+                isCorrect = (rs.getString("password")).equals(password);
             }
         } catch (SQLException ex) {
             Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return found;
+
+        return isCorrect;
     }
     
-   public boolean checkPassword(String user, String password){
-       boolean isCorrect = false;
-       if(existsUser(user)){
-           try {
-            //conectar();
-            s = connection.createStatement();
-            rs = s.executeQuery("SELECT password FROM \"USER\" WHERE user_name = '"+ user +"';");
-            while(rs.next() && !isCorrect){
-                isCorrect = ( rs.getString("password") ).equals(password);
+    public List<Task> getAllTasks(){
+        List<Task> tasks = new ArrayList<Task>();    
+        try {
+            rs = s.executeQuery("SELECT task_name, task_description, start_date, end_date FROM \"task\";");
+            while(rs.next()){
+                Task task = new Task();
+                task.setTaskName(rs.getString("task_name"));
+                task.setDescription(rs.getString("task_description"));
+                task.setStartDate(rs.getString("start_date"));
+                task.setEndDate(rs.getString("end_date"));
+                tasks.add(task);
             }
         } catch (SQLException ex) {
             Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-       }
-       return isCorrect;
-   }
+        
+        
+        return tasks;
+    }
+   
 }
